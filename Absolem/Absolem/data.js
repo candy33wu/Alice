191b241c8c1fb3db4ae3453257675a5322da7652
candy33wu <50259236+candy33wu@users.noreply.github.com>
2019-06-16 10:39:10 +0800

    Add files via upload

---
+++ Absolem/Absolem/data.js
@@ -0,0 +1,41 @@
+var questions = [
+    {
+        "question":"Who are you? ",
+        "answers":[
+            ["我是愛麗絲",2],
+            ["毛毛蟲! Absolem!!!",3]
+        ]
+    },
+    {
+        "question":"是的，妳本該知道的 傻姑娘 ",
+        "answers":[
+            ["繼續劇情",4]
+        ]
+    },
+    {
+        "question":"Who are you?",
+        "answers":[
+			["我是愛麗絲",2]
+        ]
+    },
+    {
+        "question":"The Alice will kill the dragon in frabjous day.",
+        "answers":[
+            ["那不是我 ",5],
+			["就交給我了","A"]
+        ]
+    },
+    {
+        "question":" ",
+        "answers":[
+            ["Not hardly.","B"]
+        ]
+    }
+    
+];
+
+var finalAnswers={
+        "A":["勇敢前行吧","我相信妳可以做到的，瘋帽、三月兔、柴郡貓，我們都會在冒險中陪伴著妳"],
+        "B":["Maybe she need more time. ","找回那個勇敢的妳，不要在這世界中迷失了"]
+      
+};
