708395aaf1186afa1d4a8adb11a9e60640b6fd35
candy33wu <50259236+candy33wu@users.noreply.github.com>
2019-06-17 16:53:29 +0800

    Update main.js

--- Absolem/main.js
+++ Absolem/main.js
@@ -1,5 +1,6 @@
 
 $(document).ready(function(){
+	document.getElementById("hint").style.display="none";
      document.getElementById("absolem").style.display="none";
 	document.getElementById("alice").style.display="none";
     var currentQuiz=null;
@@ -7,6 +8,7 @@ $(document).ready(function(){
         if(currentQuiz == null)
         {
 			document.getElementById("firstmeet").style.display="none";
+			document.getElementById("hint").style.display="";
 			document.getElementById("absolem").style.display="";
 			document.getElementById("alice").style.display="";
             currentQuiz=0;
